b19af54ee0d347de2f9cb0b8fa37b565b77ce093
markw53 <m.workman@outlook.com>
2025-03-11 09:41:38 +0000

    changes saved

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,5 @@
 // pages/about.tsx
-import { useRef, useEffect } from 'react'
+import { useEffect } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { motion, useAnimation } from 'framer-motion'
